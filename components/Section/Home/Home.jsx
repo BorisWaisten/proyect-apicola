521c0f1214ae3a09c0245294d1ce1dd62f73bcba
BorisWaisten <boriswaisten@gmail.com>
2024-10-17 18:08:43 -0300

    arreglando el scrolling home del touch

--- components/Section/Home/Home.jsx
+++ components/Section/Home/Home.jsx
@@ -1,23 +1,25 @@
 export default function Home() {
-    return (
-      <div id="home" className="relative w-full h-screen ">
-        {/* Video de fondo */}
-        <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
-          src="/fondoHome.mp4"
-          autoPlay
-          loop
-          muted
-        />
-  
-        {/* Contenido sobre el video */}
-        <div className="relative z-10 flex items-center justify-center h-full">
-          <h1 className="text-white text-base sm:text-lg md:text-3xl lg:text-4xl font-bold">Grupo Apícola de la Región Centro Argentina</h1>
-        </div>
-  
-        {/* Filtro oscuro (opcional) */}
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
+  return (
+    <div id="home" className="relative w-full h-screen overflow-hidden">
+      {/* Video de fondo */}
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/fondoHome3.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline  // Agrega playsInline para mejor comportamiento en móviles
+      />
+
+      {/* Contenido sobre el video */}
+      <div className="relative z-10 flex items-center justify-center h-full">
+        <h1 className="text-white text-base sm:text-lg md:text-3xl lg:text-4xl font-bold text-center">
+          Grupo Apícola de la Región Centro Argentina
+        </h1>
       </div>
-    );
-  }
-  
+
+      {/* Filtro oscuro (opcional) */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
+    </div>
+  );
+}
