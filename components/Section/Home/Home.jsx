c3637143155496bea20cf598aa78be0accd84278
BorisWaisten <boriswaisten@gmail.com>
2024-10-17 19:12:00 -0300

    .

--- components/Section/Home/Home.jsx
+++ components/Section/Home/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div id="home" className="relative w-full h-screen overflow-hidden ">
+    <div id="home" className="relative w-full h-[70vh] ">
       {/* Video de fondo */}
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover "
+        className="absolute h-[70vh] w-full object-cover"
         src="/fondoHome3.mp4"
         autoPlay
         loop
@@ -12,7 +12,7 @@ export default function Home() {
       />
 
       {/* Contenido sobre el video */}
-      <div className="relative z-10 flex items-center justify-center h-full">
+      <div className="absolute top-0 left-0 w-full z-10 flex items-center justify-center h-full">
         <h1 className="text-white text-base sm:text-lg md:text-3xl lg:text-4xl font-bold text-center">
           Grupo Apícola de la Región Centro Argentina
         </h1>
