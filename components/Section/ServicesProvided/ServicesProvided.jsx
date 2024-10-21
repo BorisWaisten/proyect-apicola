b98a7e31906ebfc26345b2a395da3565c9fdcb6a
BorisWaisten <boriswaisten@gmail.com>
2024-10-21 09:33:17 -0300

    avances services Provided

--- components/Section/ServicesProvided/ServicesProvided.jsx
+++ components/Section/ServicesProvided/ServicesProvided.jsx
@@ -1,66 +1,37 @@
 'use client';
 import React, { useState } from 'react';
-
-const services = [
-  {
-    id: 1,
-    name: 'Producción',
-    description: `Producción natural en nuestros propios apiarios. Nos comprometemos a producir garantizando una producción sana y segura mediante la prohibición de antibióticos en las colmenas y el cuidado de su buen estado nutricional.`,
-    image: '/logos/extraccion.png',
-  },
-  {
-    id: 2,
-    name: 'Extracción',
-    description: `Contamos con más de 20.000 colmenas en producción, distribuidas en la provincia de Entre Ríos, Santa Fe y Córdoba, Argentina, desde donde obtenemos nuestra propia miel.`,
-    image: '/logos/extraccion.png',
-  },
-  {
-    id: 3,
-    name: 'Laboratorio',
-    description: `Garantizamos calidad y trazabilidad en todo el proceso de producción de nuestros productos apícolas.`,
-    image: '/logos/laboratorio.png',
-  },
-  {
-    id: 4,
-    name: 'Fraccionamiento',
-    description: `Ofrecemos envases de 1.5 kg, así como tambores a granel para comercialización.`,
-    image: '/logos/extraccion.png',
-  },
-  {
-    id: 5,
-    name: 'Homogeneización',
-    description: `Homogeneizamos nuestras mieles para garantizar una calidad consistente en cada lote.`,
-    image: '/logos/extraccion.png',
-  },
-  {
-    id: 6,
-    name: 'Exportación',
-    description: `Exportamos a más de 10 destinos internacionales, miel a granel y envasada con marcas propias y marca blanca.`,
-    image: '/logos/exportacion.png',
-  },
-];
+import { services } from '@/data/section/services';
 
 export default function ServicesProvided() {
   const [activeService, setActiveService] = useState(0);
 
   return (
-    <section className="py-12">
+    <section className="py-12 min-h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-12 text-title font-title">Servicios Brindados</h2>
+        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-12 text-title font-title">
+          Servicios Brindados
+        </h2>
         <nav>
-          {/* Navegación de servicios */}
-          <ul className="flex justify-center space-x-4 flex-wrap">
+          {/* Navegación de servicios con imágenes */}
+          <ul className="grid grid-cols-3  gap-4 md:flex md:justify-center md:space-x-4 md:flex-wrap lg:flex lg:justify-center lg:space-x-4 lg:flex-wrap ">
             {services.map((service, index) => (
-              <li key={service.id} className="mb-4 sm:mb-0">
+              <li key={service.id} className="mb-4 sm:mb-0 flex justify-center">
                 <button
                   onClick={() => setActiveService(index)}
-                  className={`px-4 py-2 text-xs sm:text-sm md:text-base lg:text-lg font-semibold transition-colors duration-300 rounded-lg ${
-                    index === activeService
-                      ? 'bg-primary text-white shadow-lg'
-                      : 'text-gray-600 hover:text-primary hover:bg-terciary'
+                  className={`flex flex-col items-center justify-center transition-transform transform ${
+                    index === activeService ? 'scale-105' : 'scale-100 hover:scale-105'
                   }`}
                 >
-                  {service.name}
+                  <img
+                    src={service.image}
+                    alt={service.name}
+                    className={`w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 object-cover rounded-full border-2 ${
+                      index === activeService ? 'border-primary' : 'border-transparent hover:border-terciary'
+                    }`}
+                  />
+                    <h3 className="text-xs sm:text-sm md:text-base lg:text-lg font-semibold mt-2">
+                      {service.name}
+                    </h3>
                 </button>
               </li>
             ))}
@@ -72,15 +43,6 @@ export default function ServicesProvided() {
           <p className="text-center text-xs sm:text-sm md:text-base lg:text-lg w-[90vw] sm:w-[80vw] md:w-[70vw] lg:w-[60vw] mx-auto mb-8">
             {services[activeService].description}
           </p>
-
-          {/* Imagen del servicio */}
-          <div className="flex justify-center">
-            <img
-              className=" h-48 sm:h-64 md:h-80 lg:h-96 object-cover rounded-lg shadow-md"
-              src={services[activeService].image}
-              alt={services[activeService].name}
-            />
-          </div>
         </div>
       </div>
     </section>
---
+++ data/section/services.js
@@ -0,0 +1,39 @@
+export const services = [
+    {
+      id: 1,
+      name: 'Producción',
+      description: `Producción natural en nuestros propios apiarios. Nos comprometemos a producir garantizando una producción sana y segura mediante la prohibición de antibióticos en las colmenas y el cuidado de su buen estado nutricional.`,
+      image: '/logos/extraccion.png',
+    },
+    {
+      id: 2,
+      name: 'Extracción',
+      description: `Contamos con más de 20.000 colmenas en producción, distribuidas en la provincia de Entre Ríos, Santa Fe y Córdoba, Argentina, desde donde obtenemos nuestra propia miel.`,
+      image: '/logos/extraccion.png',
+    },
+    {
+      id: 3,
+      name: 'Laboratorio',
+      description: `Garantizamos calidad y trazabilidad en todo el proceso de producción de nuestros productos apícolas.`,
+      image: '/logos/laboratorio.png',
+    },
+    {
+      id: 4,
+      name: 'Fraccionamiento',
+      description: `Ofrecemos envases de 1.5 kg, así como tambores a granel para comercialización.`,
+      image: '/logos/extraccion.png',
+    },
+    {
+      id: 5,
+      name: 'Homogeneización',
+      description: `Homogeneizamos nuestras mieles para garantizar una calidad consistente en cada lote.`,
+      image: '/logos/extraccion.png',
+    },
+    {
+      id: 6,
+      name: 'Exportación',
+      description: `Exportamos a más de 10 destinos internacionales, miel a granel y envasada con marcas propias y marca blanca.`,
+      image: '/logos/exportacion.png',
+    },
+  ];
+  
