af72202db57baa2091bc3cbffb89466e04740f46
BorisWaisten <boriswaisten@gmail.com>
2024-10-31 18:35:50 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                ],
+            },
+        ];
+    }
 };
 
 export default nextConfig;
