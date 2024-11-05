3b5328c2c46c820de18949f66cf6e564088a1bbf
BorisWaisten <boriswaisten@gmail.com>
2024-11-05 19:45:33 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
             source: "/api/:path*",  // Coincide con todas las rutas de API
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" }, // Permitir credenciales
-              { key: "Access-Control-Allow-Origin", value: "*" }, // Permitir todas las orígenes (mejor especificar un dominio en producción)
+              { key: "Access-Control-Allow-Origin", value: "https://argentinehoney.com" }, // Permitir todas las orígenes (mejor especificar un dominio en producción)
               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" }, // Métodos permitidos
               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }, // Encabezados permitidos
             ]
