4fc1624c1e3158e0c7e28b79b7a8f84a3d7d58c1
BorisWaisten <boriswaisten@gmail.com>
2024-11-05 20:37:13 -0300

    cambios de metadata

--- app/page.js
+++ app/page.js
@@ -1,32 +1,22 @@
-'use client';
-import { useEffect, useState } from "react";
 import About from "@/components/Section/About/About";
 import Contact from "@/components/Section/Contact/Contact";
 import Home from "@/components/Section/Home/Home";
 import Varieties from "@/components/Section/Varieties/Varieties";
-import Loading from "@/components/Loading/Loading"; // Importar el componente de loading
 import ServicesProvided from "@/components/Section/ServicesProvided/ServicesProvided";
 import Biome from "@/components/Section/Biome/Biome";
 import Production from "@/components/Section/Production/Production";
 import Certifications from "@/components/Section/Certifications/Certifications";
 import CompanySupport from "@/components/Section/CompanySupport/CompanySupport";
+import { metadata } from "./layout";
 
+export const metadataHome = {
+  ...metadata,
+  icons:{
+    icon: "/logoNavBar.jpg",
+  }
+}
 
 export default function HomePage() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  // Usamos useEffect para controlar el tiempo de carga
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false); // Después de 2 segundos, quitamos la pantalla de carga
-    }, 2000); // 2 segundos de tiempo de carga
-
-    return () => clearTimeout(timer); // Limpiamos el temporizador cuando el componente se desmonte
-  }, []);
-
-  if (isLoading) {
-    return <Loading />; // Mostrar la pantalla de carga
-  }
 
   return (
     <div>
